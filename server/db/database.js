2e5291360270f666c70a461c3942ff18fabf435b
jahedprince <jahedprince183@gmail.com>
2023-08-03 19:07:46 -0400

    Fix SSL certificate issue in Sequelize configuration

--- server/db/database.js
+++ server/db/database.js
@@ -1,8 +1,8 @@
 // The sole purpose of this module is to establish a connection to your
 // Postgres database by creating a Sequelize instance (called `db`).
 // You shouldn't need to make any modifications here.
-const Sequelize = require("sequelize");
-const pkg = require("../../package.json");
+const { Sequelize } = require("sequelize");
+const pkg = require("./package.json");
 
 let db;
 
@@ -10,6 +10,11 @@ if (process.env.NODE_ENV === "production") {
   // On Heroku, use the provided DATABASE_URL environment variable
   db = new Sequelize(process.env.DATABASE_URL, {
     logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   });
 } else {
   // In development, use the local database URL
@@ -17,5 +22,7 @@ if (process.env.NODE_ENV === "production") {
     logging: false,
   });
 }
+
+// Rest of the Sequelize configuration and model definitions go here
 
 module.exports = db;
